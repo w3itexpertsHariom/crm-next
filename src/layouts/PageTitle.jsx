ceaead90e6053955c9e29d9685773d246d899bd0
Hariom <hariom.w3itexperts@gmail.com>
2024-11-20 12:44:31 +0530

    rm grid

--- src/layouts/PageTitle.jsx
+++ src/layouts/PageTitle.jsx
@@ -1,27 +1,30 @@
-"use client"
+"use client";
+
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const PageTitle = ({ motherMenu, activeMenu }) => {
-	const getPath = usePathname();
-  	const [path, setPath] = useState([]);
-	  useEffect(() => {
-		if (getPath) {
-		  setPath(getPath.split("/"));
-		}
-	  }, [getPath]);
+  const getPath = usePathname();
+  const [path, setPath] = useState([]);
 
+  useEffect(() => {
+    if (getPath) {
+      setPath(getPath.split("/"));
+    }
+  }, [getPath]);
   return (
-		<>
-			<div className="row page-titles mx-0">
-				<ol className="breadcrumb">
-					<li className="breadcrumb-item"><Link href={`/${path[path.length - 2] || ""}`}>{motherMenu}</Link></li>
-					<li className="breadcrumb-item active"><Link href={`/${path[path.length - 1] || ""}`}>{activeMenu}</Link></li>
-				</ol>
-			</div>
-		</>
-    	
+    <div className="row page-titles mx-0">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <Link href={`/${path[path.length - 2] || ""}`}>{motherMenu}</Link>
+        </li>
+        <li className="breadcrumb-item active">
+          <Link href={`/${path[path.length - 1] || ""}`}>{activeMenu}</Link>
+        </li>
+      </ol>
+    </div>
   );
 };
+
 export default PageTitle;
