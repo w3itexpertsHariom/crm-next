8e95b6c826e68e3a338ffbab7971bb8e1d9b15ff
Hariom <hariom.w3itexperts@gmail.com>
2024-11-20 16:24:30 +0530

    ddc

--- src/app/(email)/app-calender/page.jsx
+++ src/app/(email)/app-calender/page.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import PageTitle from "@/layouts/PageTitle";
 import dynamic from 'next/dynamic';
 
-// const EventCalendar = dynamic(() => import('@/components/Calendar/EventCalendar'), { ssr: false });
+const EventCalendar = dynamic(() => import('@/components/Calendar/EventCalendar'), { ssr: false });
 
 const Calendar = () => {
    return (
       <div className="h-80">
          <PageTitle activeMenu="Calerdar" motherMenu="App" />
          <div className="container-fluid">
-            {/* <EventCalendar /> */}
+            <EventCalendar />
          </div>
       </div>
    );
